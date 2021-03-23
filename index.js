284a10033575db45462c159dcd4360afd78eb914
ahsanarique <ahsan.arique@gmail.com>
2021-03-23 21:11:30 +0600

    basics

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+const users = [
+  "ahfvjash",
+  "ajdstyfd",
+  "ahfwiu",
+  "auykdfy",
+  "ayfdyaes",
+  "trqwecjfn",
+];
+
+const port = 3000;
+
 app.get("/", (req, res) => {
-  res.send("calling root");
+  res.send(`calling root at port ${port}`);
 });
 
-app.listen(3000, () => console.log("listening to port 3000"));
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  const name = users[id];
+  res.send({ id, name });
+});
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
